a2cf6ee735d494d1a9cc47aacb1da9e8c652d888
David Halewood <davidnhalewood@hotmail.com>
2017-09-12 13:36:18 +0100

    adding hobbies and height whilst recentering text

--- directory.rb
+++ directory.rb
@@ -2,36 +2,43 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  # create an empty array
   students = []
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
+    hobby_list = [
+      "chess",
+      "revenge",
+      "general evilness",
+      "knitting",
+      "concocting plans to take over the world",
+      "butterfly collecting",
+      "resurrecting the dead",
+      "swimming",
+      "coding",
+      "designing killer weapons",
+      "browsing youtube for hours and hours"
+    ]
+    students << {name: name, cohort: :november, hobbies: hobby_list[rand(0..10)], height: "#{rand(4..6)} foot and #{rand(1..12)} inches"}
     puts "Now we have #{students.count} students"
-    # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(40)
-  puts "-------------".center(40)
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 
 def print(students)
   students.each_with_index do |student, i|
-      puts "#{i+1}: #{student[:name]} (#{student[:cohort]} cohort)".center(40)
+      puts "#{i+1}: #{student[:name]} is in the #{student[:cohort]} cohort, likes #{student[:hobbies]} and is #{student[:height]} tall.".center(100)
   end
 end
 
 def print_footer(students)
-  puts "-------------".center(40)
-  puts "Overall, we have #{students.count} great students".center(40)
+  puts "-------------".center(100)
+  puts "Overall, we have #{students.count} great students".center(100)
 end
 
 # nothing happens until we call the methods
