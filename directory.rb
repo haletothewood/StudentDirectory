279839e9e4161fd459e22e00989ffe6a21cb0ed6
David Halewood <davidnhalewood@hotmail.com>
2017-09-13 21:14:05 +0100

    adding ability to save to file

--- directory.rb
+++ directory.rb
@@ -13,6 +13,8 @@ def process(selection)
       input_students
     when "2"
       print_students_list
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -24,6 +26,7 @@ def print_menu
   puts "What would you like to do?"
   puts "1. Input the students"
   puts "2. Show the student directory"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -33,10 +36,6 @@ def prompt
   if !@name.empty?
     puts "What cohort are they in?"
     @cohort = gets.chop.capitalize
-    puts "What do they like to do?"
-    @hobby = gets.chop
-    puts "And finally, what is their height in feet and inches?"
-    @height = gets.chop
   end
 end
 
@@ -44,7 +43,7 @@ def input_students
   prompt
   while !@name.empty? do
 
-  @students << {name: @name, cohort: @cohort, hobbies: @hobby, height: @height}
+  @students << {name: @name, cohort: @cohort}
   if @students.count == 1
     puts "Now we have #{@students.count} student"
   else
@@ -53,6 +52,18 @@ def input_students
   prompt
   end
   @students
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+  puts "Your file has been saved."
+  puts "-------------"
 end
 
 def print_header
@@ -73,10 +84,12 @@ def print_students_list
   if @students != []
     print_header
     @students.each_with_index do |student, i|
-      puts "#{i+1}: #{student[:name]}".ljust(20) + "(#{student[:cohort]} Cohort)".center(20) + "They like to #{student[:hobbies]} and are #{student[:height]} tall.".center(35)
+      puts "#{i+1}: #{student[:name]}".ljust(20) + "(#{student[:cohort]} Cohort)".center(20)
     end
     print_footer
   end
 end
+
+
 
 interactive_menu
