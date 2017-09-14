73ca5ad371ac89dc9015ec47103614cc88ea68a4
David Halewood <davidnhalewood@hotmail.com>
2017-09-14 17:48:33 +0100

    updating print_me.rb to print it's own code

--- print_me.rb
+++ print_me.rb
@@ -1,1 +1,1 @@
-puts __FILE__
+def method;"def method;;end;puts method()[0, 11] + 34.chr + method + 34.chr + method()[11, method.length-11]";end;puts method()[0, 11] + 34.chr + method + 34.chr + method()[11, method.length-11]
