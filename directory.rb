666ca02214c948b09161d451b0a3d49445b9e8c9
David Halewood <davidnhalewood@hotmail.com>
2017-09-14 12:46:52 +0100

    setting students.csv as default on startup

--- directory.rb
+++ directory.rb
@@ -83,8 +83,10 @@ end
 
 def try_load_students
   filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
+  if filename.nil?
+    load_students("students.csv")
+    puts "Loaded #{@students.count} from students.csv"
+  elsif File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else
